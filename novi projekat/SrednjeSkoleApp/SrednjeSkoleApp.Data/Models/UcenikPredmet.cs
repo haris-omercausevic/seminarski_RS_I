05e7cf879a4b160e1c2772d5100ee8d161393eb9
Haris Omerčaušević <haris@edu.fit.ba>
2018-03-12 22:19:53 +0100

    a

--- novi projekat/SrednjeSkoleApp/SrednjeSkoleApp.Data/Models/UcenikPredmet.cs
+++ novi projekat/SrednjeSkoleApp/SrednjeSkoleApp.Data/Models/UcenikPredmet.cs
@@ -10,6 +10,10 @@ namespace SrednjeSkoleApp.Data.Models
         public double? ZakljucnaOcjena { get; set; ***REMOVED***
         public int PredmetId { get; set; ***REMOVED***
         public virtual Predmet Predmet { get; set; ***REMOVED***
+
+        public int NastavnikId { get; set; ***REMOVED***
+        public virtual Nastavnik Nastavnik { get; set; ***REMOVED***
+
         public int UcenikId { get; set; ***REMOVED***
         public virtual Ucenik Ucenik { get; set; ***REMOVED***
 
